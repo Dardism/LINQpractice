03e2c8b0bb28ab9fbe24d0b39f710ec41a7c2d95
Michael <dardismm@gmail.com>
2017-10-27 16:36:04 -0400

    test

--- FunctionalProgramming/FunctionalProgramming/Program.cs
+++ FunctionalProgramming/FunctionalProgramming/Program.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace FunctionalProgramming {
   class Program {
+
+    delegate void SayGreeting(string name);
+
+    public static void SayGoodbye(string name) {
+      Console.WriteLine(string.Format("Later, {0}", name));
+    }
+
     static void Main(string[] args) {
 
+      SayGreeting sayGreeting = delegate(string name) {
+        Console.WriteLine(string.Format("Hello, {0}", name));
+      };
+
+      Console.WriteLine("What's your name?");
+      string input = Console.ReadLine();
+      sayGreeting(input);
+
+      sayGreeting = new SayGreeting(SayGoodbye);
 
     }
   }
